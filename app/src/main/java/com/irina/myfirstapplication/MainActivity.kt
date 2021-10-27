cd3550a4640f2c51fe8634a1cd327d8e70772894
Ирина <is.bykova04@gmail.com>
2021-10-27 17:26:30 +0500

    log even letter english alphabet

--- app/src/main/java/com/irina/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/irina/myfirstapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.irina.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val letters = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+        letters.forEach {
+            if (letters.indexOf(it) % 2 == 1)
+                Log.d("letter", it)
+        }
     }
 }
